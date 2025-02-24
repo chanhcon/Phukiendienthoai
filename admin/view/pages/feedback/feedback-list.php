56a9f015cdc55c3c1791d16bf2fc4547408e7392
Longdeptrai8386 <longvhpn00080@gmail.com>
2025-02-24 11:37:17 +0700

    .

--- admin/view/pages/feedback/feedback-list.php
+++ admin/view/pages/feedback/feedback-list.php
@@ -1,17 +1,15 @@
-
-
 <div class="card">
     <div class="card-body">
         <div class="d-flex align-items-center">
             <h5 class="mb-0">Danh sách feedback người dùng</h5>
-            <form class="ms-auto position-relative" method="POST">
+            <form class="ms-auto position-relative">
                 <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i class="bi bi-search"></i>
                 </div>
-                <input class="form-control ps-5" type="text" name="search" placeholder="Tìm kiếm feedback" value="<?php echo htmlspecialchars($search_query, ENT_QUOTES, 'UTF-8'); ?>">
+                <input class="form-control ps-5" type="text" placeholder="search">
             </form>
         </div>
-        <div class="mt-3 table-responsive">
-            <table class="table align-middle table-hover">
+        <div class=" mt-3">
+            <table style="" class="table align-middle table-hover">
                 <thead class="table-secondary">
                     <tr>
                         <th scope="col">#ID</th>
@@ -26,25 +24,36 @@
                 </thead>
                 <tbody>
                     <?php
-                    foreach ($feedback_list as $feedback) {
-                        echo '
-                        <tr>
-                            <td scope="row">#' . htmlspecialchars($feedback['id'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td>' . htmlspecialchars($feedback['name'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td>' . htmlspecialchars($feedback['email'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td>' . htmlspecialchars($feedback['phone'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td>' . htmlspecialchars($feedback['title'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td style="width: 200px; word-wrap: break-word">' . htmlspecialchars($feedback['content'], ENT_QUOTES, 'UTF-8') . '</td>
-                            <td>' . date('d-m-Y H:i:s', strtotime($feedback['date_create'])) . '</td>
-                            <td style="cursor: pointer">
-                                <a href="reply-feedback.php?id=' . $feedback['id'] . '" style="font-weight: 550;">Reply</a>
-                            </td>
-                        </tr>';
-                    }
-                    ?>
+$feedback_list = select_all_feedback_list();
+
+foreach ($feedback_list as $feedback) {
+
+    echo '
+                    <tr>
+                        <td scope="row">#' . $feedback['id'] . '</td>
+                        <td>
+                            ' . $feedback['name'] . '
+                        </td>
+                        <td>' . $feedback['email'] . '</td>
+                        <td>' . $feedback['phone'] . '</td>
+                        <td>' . $feedback['title'] . '</td>
+                        <td style="width: 200px; word-wrap: break-word">' . $feedback['content'] . '</td>
+                        <td>' . $feedback['date_create'] . '</td>
+                        <td style="cursor: pointer">
+                        <a href="#!" style="font-weight: 550;">Reply</a>
+                        </td>
+
+
+                    </tr>
+    ';
+}
+?>
                 </tbody>
             </table>
         </div>
+    </div>
+</div>
 
-        <!-- Pagination -->
-        <nav>
+
+</main>
+<!--end page main-->
