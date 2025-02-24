0133606b60148c5d4a152cad3d74a08e3f1b8436
Longdeptrai8386 <longvhpn00080@gmail.com>
2025-02-24 11:06:12 +0700

    giao diên

--- admin/auth/login.php
+++ admin/auth/login.php
@@ -8,10 +8,9 @@ include "../models/user.php";
 include "../../global.php";
 include "$ROOT_URL/pdo-library.php";
 include "$ROOT_URL/DAO/user.php";
-
 ?>
-<?php
 
+<?php
 if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
     $error = array();
     $email = $_POST['email'];
@@ -19,32 +18,23 @@ if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
     // Đối chiếu password
     //Validate form
     if (empty($email)) {
-        $error['email'] = "Không để trống email";
+        $error['email'] = "Không để trống email!";
     }
     if (empty($password)) {
-        $error['password'] = "Không để trống password";
+        $error['password'] = "Không để trống password!";
     }
-    // echo $email;
-    // echo $password;
     if (!$error) {
         $password = md5($password);
-        // echo $password;
         $islogined = checkuser2($email, $password);
-        // echo $islogined;
         if ($islogined === -1) {
-            echo '
-                <script>
-                    window.alert("Email hoặc mật khẩu của bạn không đúng. Xin vui lòng nhập lại!");
-                </script>
-                ';
+            echo '<div class="alert-warning alert text-center" style="">Email hoặc password không chính xác</div>';
         } else {
             $kq = getuserinfo2($email, $password);
             $role = $kq[0]['vai_tro'];
-            // echo $role;
             if ($role == 1 || $role == 2) {
                 $_SESSION['role'] = $role;
                 $_SESSION['username'] = $kq[0]['ho_ten'];
-                $_SESSION['idadmin'] = $kq[0]['id'];
+                $_SESSION['iduser'] = $kq[0]['id'];
                 $_SESSION['img'] = $kq[0]['hinh_anh'];
                 header('Location: ../index.php');
             } else {
@@ -52,13 +42,12 @@ if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
                     <script>
                         window.alert("Email hoặc mật khẩu của bạn không đúng. Xin vui lòng nhập lại!");
                     </script>
-                    ';
+                ';
             }
         }
     }
 }
 ?>
-
 
 <!doctype html>
 <html lang="en">
@@ -79,32 +68,75 @@ if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
     <!-- loader-->
     <link href="../assets/css/pace.min.css" rel="stylesheet" />
     <style>
-        .btn-primary-login {
-            background-color: #ff7f00;
-            color: white;
-            border: none;
+        body {
+            background-color: #f3f4f7;
         }
 
-        .btn-primary-login:hover {
-            background-color: #ff7f00;
-            border: none;
+        .wrapper {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
         }
 
-        .mb-0 a {
+        .authentication-card {
+            width: 100%;
+            max-width: 450px;
+            background-color: #fff;
+            border-radius: 8px;
+            box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .card-body {
+            padding: 3rem;
+        }
+
+        .card-title {
+            font-size: 24px;
+            font-weight: bold;
+            text-align: center;
             color: #ff7f00;
         }
 
-        .form-check-input-change:checked {
-            border: #ff7f00;
+        .form-control {
+            border-radius: 30px;
+            padding-left: 40px;
+            font-size: 16px;
+        }
+
+        .form-control:focus {
+            border-color: #ff7f00;
+            box-shadow: 0 0 5px rgba(255, 127, 0, 0.5);
+        }
+
+        .search-icon {
+            position: absolute;
+            top: 50%;
+            left: 10px;
+            transform: translateY(-50%);
+        }
+
+        .btn-primary-login {
             background-color: #ff7f00;
+            color: white;
+            border-radius: 30px;
+            font-weight: bold;
+            transition: background-color 0.3s;
+        }
+
+        .btn-primary-login:hover {
+            background-color: #d56900;
+            border: none;
+        }
+
+        .form-check-label {
+            font-size: 14px;
+            color: #ff7f00;
         }
 
         .text-end a {
             color: #ff7f00;
-        }
-
-        .images img {
-            width: 70%;
+            font-weight: bold;
         }
 
         .error-message-login {
@@ -118,156 +150,107 @@ if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
             color: red;
             font-weight: 550;
         }
+
+        .images img {
+            width: 100%;
+            border-radius: 10px;
+        }
+
+        .login-separater {
+            text-align: center;
+            margin: 1.5rem 0;
+            position: relative;
+            font-weight: bold;
+            color: #777;
+        }
+
+        .login-separater hr {
+            border: 1px solid #ff7f00;
+            width: 50%;
+            margin: auto;
+        }
+
+        .login-separater span {
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            background-color: #fff;
+            padding: 0 10px;
+            top: -12px;
+        }
     </style>
 
     <title>Admin Golden Bee Group</title>
-
 </head>
 
 <body>
 
-    <!--start wrapper-->
     <div class="wrapper">
+        <div class="authentication-card">
+            <div class="row g-0">
+                <div class="col-lg-6 d-flex align-items-center justify-content-center images">
+                    <img src="../assets/images/error/login-admin.png" class="img-fluid" alt="login-image">
+                </div>
+                <div class="col-lg-6">
+                    <div class="card-body p-4 p-sm-5">
+                        <h5 class="card-title">Đăng nhập</h5>
+                        <p class="card-text mb-5 text-center">Đăng nhập để vào trang quản trị admin</p>
+                        <form class="form-body" id="form-login-admin" action="./login.php" method="post">
+                            <input type="hidden" name="csrf_token" value="<?= generateCSRFToken() ?>">
 
-        <?php
+                            <div class="row g-3">
 
-        if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
-            $error = array();
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            // Đối chiếu password
-            //Validate form
-            if (empty($email)) {
-                $error['email'] = "Không để trống email!";
-            }
-            if (empty($password)) {
-                $error['password'] = "Không để trống password!";
-            }
-            // echo $email;
-            // echo $password;
-            if (!$error) {
-                $password = md5($password);
-                // echo $password;
-                $islogined = checkuser2($email, $password);
-                // echo $islogined;
-                if ($islogined === -1) {
-                    echo '<div class="alert-warning alert text-center" style="">Email hoặc password không chính xác</div>';
-                    // $_SESSION['toastAlert'] = "Email hoặc password không chính xác";
-                } else {
-                    $kq = getuserinfo2($email, $password);
-                    $role = $kq[0]['vai_tro'];
-                    // echo $role;
-                    if ($role == 1 || $role == 2) {
-                        $_SESSION['role'] = $role;
-                        $_SESSION['username'] = $kq[0]['ho_ten'];
-                        $_SESSION['iduser'] = $kq[0]['id'];
-                        $_SESSION['img'] = $kq[0]['hinh_anh'];
-                        header('Location: ../index.php');
-                    } else {
-                        echo '
-                    <script>
-                        window.alert("Email hoặc mật khẩu của bạn không đúng. Xin vui lòng nhập lại!");
-                    </script>
-                    ';
-                    }
-                }
-            }
-        }
-        ?>
-
-        <!--start content-->
-        <main class="authentication-content">
-            <div class="container-fluid">
-                <div class="authentication-card">
-                    <div class="card shadow rounded-0 overflow-hidden">
-                        <div class="row g-0">
-                            <div class="col-lg-6 d-flex align-items-center justify-content-center images">
-                                <img src="../assets/images/error/login-admin.png" class="img-fluid" alt="">
-                            </div>
-                            <div class="col-lg-6">
-                                <div class="card-body p-4 p-sm-5">
-                                    <h5 class="card-title">Đăng nhập</h5>
-                                    <p class="card-text mb-5">Đăng nhập để vào trang quản trị admin</p>
-                                    <form class="form-body" id="form-login-admin" action="./login.php" method="post">
-                                        <!-- <div class="d-grid">
-                                            <a class="btn btn-white radius-30" href="javascript:;"><span
-                                                    class="d-flex justify-content-center align-items-center">
-                                                    <img class="me-2" src="../assets/images/icons/search.svg" width="16"
-                                                        alt="">
-                                                    <span>Sign in with Google</span>
-                                                </span>
-                                            </a>
+                                <div class="col-12">
+                                    <label for="inputEmailAddress" class="form-label">Địa chỉ email </label>
+                                    <div class="ms-auto position-relative">
+                                        <div class="position-absolute top-50 translate-middle-y search-icon px-3">
+                                            <i class="bi bi-envelope-fill"></i>
                                         </div>
-                                        <div class="login-separater text-center mb-4"> <span>OR SIGN IN WITH
-                                                EMAIL</span>
-                                            <hr>
-                                        </div> -->
-                                        <div class="row g-3">
-
-                                            <div class="col-12">
-                                                <label for="inputEmailAddress" class="form-label">Địa chỉ email </label>
-                                                <div class="ms-auto position-relative">
-                                                    <div
-                                                        class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                        <i class="bi bi-envelope-fill"></i>
-                                                    </div>
-                                                    <input type="email" class="form-control radius-30 ps-5"
-                                                        id="inputEmailAddress" placeholder="Email" name="email">
-                                                </div>
-                                                <p class="error-message-login">
-                                                    <?php echo isset($error['email']) ? $error['email'] : ''; ?></p>
-                                            </div>
-
-                                            <div class="col-12">
-                                                <label for="inputChoosePassword" class="form-label">Nhập mật
-                                                    khẩu</label>
-                                                <div class="ms-auto position-relative">
-                                                    <div
-                                                        class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                        <i class="bi bi-lock-fill"></i>
-                                                    </div>
-                                                    <input type="password" class="form-control radius-30 ps-5"
-                                                        id="inputChoosePassword" placeholder="Mật khẩu" name="password">
-                                                </div>
-                                                <p class="error-message-login">
-                                                    <?php echo isset($error['password']) ? $error['password'] : ''; ?>
-                                                </p>
-                                            </div>
-                                            <div class="col-6">
-                                                <div class="form-check form-switch">
-                                                    <input class="form-check-input form-check-input-change"
-                                                        type="checkbox" id="flexSwitchCheckChecked" checked="">
-                                                    <label class="form-check-label" for="flexSwitchCheckChecked">Nhớ
-                                                        thông tin</label>
-                                                </div>
-                                            </div>
-                                            <div class="col-6 text-end"> <a href="./forgot.php">Quên mật khẩu?</a>
-                                            </div>
-                                            <div class="col-12">
-                                                <div class="d-grid">
-                                                    <input type="submit" name="loginbtn"
-                                                        class="btn btn-primary radius-30 btn-primary-login"
-                                                        value="Đăng nhập" />
-                                                </div>
-                                            </div>
-                                            <!-- <div class="col-12">
-                                                <p class="mb-0">Vẫn chưa có tài khoản ? <a href="./">Đăng ký tại đây</a>
-                                                </p>
-                                            </div> -->
-                                        </div>
-                                    </form>
+                                        <input type="email" class="form-control ps-5" id="inputEmailAddress" placeholder="Email" name="email">
+                                    </div>
+                                    <p class="error-message-login">
+                                        <?php echo isset($error['email']) ? $error['email'] : ''; ?></p>
                                 </div>
+
+                                <div class="col-12">
+                                    <label for="inputChoosePassword" class="form-label">Nhập mật khẩu</label>
+                                    <div class="ms-auto position-relative">
+                                        <div class="position-absolute top-50 translate-middle-y search-icon px-3">
+                                            <i class="bi bi-lock-fill"></i>
+                                        </div>
+                                        <input type="password" class="form-control ps-5" id="inputChoosePassword" placeholder="Mật khẩu" name="password">
+                                    </div>
+                                    <p class="error-message-login">
+                                        <?php echo isset($error['password']) ? $error['password'] : ''; ?>
+                                    </p>
+                                </div>
+
+                                <div class="col-6">
+                                    <div class="form-check form-switch">
+                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked="">
+                                        <label class="form-check-label" for="flexSwitchCheckChecked">Nhớ thông tin</label>
+                                    </div>
+                                </div>
+                                <div class="col-6 text-end"> <a href="./forgot.php">Quên mật khẩu?</a></div>
+
+                                <div class="col-12">
+                                    <div class="d-grid">
+                                        <input type="submit" name="loginbtn" class="btn btn-primary btn-primary-login" value="Đăng nhập" />
+                                    </div>
+                                </div>
+
+                                <div class="col-12">
+                                    <p class="mb-0 text-center">Vẫn chưa có tài khoản ? <a href="./">Đăng ký tại đây</a></p>
+                                </div>
+
                             </div>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
-        </main>
-
-        <!--end page main-->
-
+        </div>
     </div>
-    <!--end wrapper-->
+
     <!-- Bootstrap bundle JS -->
     <script src="../assets/js/bootstrap.bundle.min.js"></script>
     <!--plugins-->
@@ -278,9 +261,6 @@ if (isset($_POST['loginbtn']) && $_POST['loginbtn']) {
     </script>
 
     <script src="../assets/js/pages/validate.js"></script>
-
-
-
 
 </body>
 
